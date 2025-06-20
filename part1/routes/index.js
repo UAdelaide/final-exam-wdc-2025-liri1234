53fee9647bc6a25e68cd0de9c6b7b31af824e06c
Subhashree <unknown@student.adelaide.edu.au>
2025-06-20 02:06:07 +0000

    [automatic commit] 2025-06-20 02:06:07 UTC

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
-const db = page. */
+const db = require('')
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
