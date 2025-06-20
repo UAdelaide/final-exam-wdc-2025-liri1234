e42d23d9a65cbe9a9144ec86834239c7daaea215
Subhashree <unknown@student.adelaide.edu.au>
2025-06-20 08:02:51 +0000

    [automatic commit] 2025-06-20 08:02:51 UTC

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -50,6 +50,7 @@ router.post('/login', async (req, res) => {
     }
 
     res.json({ message: 'Login successful', user: rows[0] });
+    res.session.user = rows[0];
   } catch (error) {
     res.status(500).json({ error: 'Login failed' });
   }
