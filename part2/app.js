5bf90347c2b27c9ea2d56e3c67e953baa0b59d4a
Subhashree <unknown@student.adelaide.edu.au>
2025-06-20 08:59:05 +0000

    [automatic commit] 2025-06-20 08:59:05 UTC

--- part2/app.js
+++ part2/app.js
@@ -13,6 +13,7 @@ app.use(express.static(path.join(__dirname, '/public')));
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
 
+//using sessions
 app.use(session({
   secret: 'dogwalker_secret',
   resave: false,
