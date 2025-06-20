be8e3184ccaf549062614a7dd15c0ad115483126
Subhashree <unknown@student.adelaide.edu.au>
2025-06-20 02:32:18 +0000

    [automatic commit] 2025-06-20 02:32:18 UTC

--- part1/app.js
+++ part1/app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mysql = require('mysql2/promise');
+
 
 
 var indexRouter = require('./routes/index');
