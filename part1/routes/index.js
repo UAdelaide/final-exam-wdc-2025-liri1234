19cf415a0b06e39ec457b2d72d81c14e35297d2f
Subhashree <unknown@student.adelaide.edu.au>
2025-06-20 02:40:11 +0000

    [automatic commit] 2025-06-20 02:40:11 UTC

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -18,7 +18,15 @@ router.get('/api/dogs', function(req, res, next) {
     }
 });
 router.get('/api/walkrequests/open', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  try {
+        const [rows] = await db.query(
+            'SELECT wr.request_id, d.name AS dog_name, wr.requested_time, wr.duration_minutes, wr.location, u.username AS owner_username FROM WalkRequests wr JOIN Dogs d ON wr.dog_id = d.dog_id JOIN  Users u ON d.owner_id = u.user_id WHERE wr.status = "open"'
+        );
+
+        res.send(rows);
+    } catch (err) {
+        res.sendStatus(500);
+    }
 });
 router.get('/api/walkers/summary', function(req, res, next) {
   res.render('index', { title: 'Express' });
