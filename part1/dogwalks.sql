c15762e85db189f0075eb0221c5cc619cb7aea92
Subhashree <unknown@student.adelaide.edu.au>
2025-06-20 01:33:23 +0000

    [automatic commit] 2025-06-20 01:33:23 UTC

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -54,14 +54,13 @@ CREATE TABLE WalkRatings (
     CONSTRAINT unique_rating_per_walk UNIQUE (request_id)
 );
 
--- Insert 5 users
 INSERT INTO Users (username, email, password_hash, role)
 VALUES
 ('alice123', 'alice@example.com', 'hashed123', 'owner'),
 ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
 ('carol123', 'carol@example.com', 'hashed789', 'owner'),
 ('liridas', 'liri@example.com', 'hashed021', 'walker'),
-('jenna123', 'emily@example.com', 'hashed569', 'owner');
+('jenna123', 'jenna@example.com', 'hashed569', 'owner');
 
 -- Insert 5 dogs using subqueries to find owner_id
 INSERT INTO Dogs (owner_id, name, size)
