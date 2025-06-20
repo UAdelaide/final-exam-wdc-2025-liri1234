8df1ba20f7ad164956df806016565236e04d3c65
Subhashree <unknown@student.adelaide.edu.au>
2025-06-20 02:48:08 +0000

    [automatic commit] 2025-06-20 02:48:08 UTC ;; communications error to 208.67.222.222#53: timed out

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -18,7 +18,7 @@ router.get('/api/dogs', async function(req, res, next) {
     }
 });
 
-router.get('/api/walkrequests/open', function(req, res, next) {
+router.get('/api/walkrequests/open',async function(req, res, next) {
   try {
         const [rows] = await db.query(
             'SELECT wr.request_id, d.name AS dog_name, wr.requested_time, wr.duration_minutes, wr.location, u.username AS owner_username FROM WalkRequests wr JOIN Dogs d ON wr.dog_id = d.dog_id JOIN  Users u ON d.owner_id = u.user_id WHERE wr.status = "open"'
