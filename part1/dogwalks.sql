50375d2b57fa2b44421cf256c42e157f0526d380
Subhashree <unknown@student.adelaide.edu.au>
2025-06-20 01:36:28 +0000

    [automatic commit] 2025-06-20 01:36:28 UTC

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -68,7 +68,8 @@ VALUES
 ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Max', 'medium'),
 ((SELECT user_id FROM Users WHERE username = 'carol123'), 'Bella', 'small'),
 ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Rocky', 'large'),
-((SELECT user_id FROM Users WHERE username = 'liri123'), 'Luna', 'medium'),
+((SELECT user_id FROM Users WHERE username = 'liridas'), 'Luna', 'medium'),
+((SELECT user_id FROM Users WHERE username = 'liridas'), 'Luna', 'medium'),
 ((SELECT user_id FROM Users WHERE username = 'carol123'), 'Milo', 'small');
 
 INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status)
