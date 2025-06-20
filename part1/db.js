01adb3133e29a4506c6db5605b6633f895b79927
Subhashree <unknown@student.adelaide.edu.au>
2025-06-20 02:03:02 +0000

    [automatic commit] 2025-06-20 02:03:02 UTC

--- part1/db.js
+++ part1/db.js
@@ -1,13 +1,9 @@
 const mysql = require('mysql2/promise');
-require('dotenv').config();
-
-const { DB_HOST, DB_USER, DB_PASS, DB_NAME } = process.env;
 
 const pool = mysql.createPool({
-  host: DB_HOST,
-  user: DB_USER,
-  password: DB_PASS,
-  database: DB_NAME,
+  host: '127.0.0.1',
+  user: 'root',
+  password: '',
+  database: 'DogWalkService'
 });
-
 module.exports = pool;
