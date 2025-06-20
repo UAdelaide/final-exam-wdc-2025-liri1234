ed0cd4ad6bf917b54b84011dbaa86d75eff4c123
Subhashree <unknown@student.adelaide.edu.au>
2025-06-20 01:41:37 +0000

    [automatic commit] 2025-06-20 01:41:37 UTC

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -67,7 +67,7 @@ INSERT INTO Dogs (owner_id, name, size)
 VALUES
 ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Max', 'medium'),
 ((SELECT user_id FROM Users WHERE username = 'carol123'), 'Bella', 'small'),
-((SELECT user_id FROM Users WHERE username = 'alice123'), '', 'large'),
+((SELECT user_id FROM Users WHERE username = 'alice123'), 'chicken', 'large'),
 ((SELECT user_id FROM Users WHERE username = 'liridas'), 'Cookies', 'medium'),
 ((SELECT user_id FROM Users WHERE username = 'jenna123'), 'Bull', 'medium'),
 ((SELECT user_id FROM Users WHERE username = 'carol123'), 'Sweety', 'small');
@@ -76,7 +76,7 @@ INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, st
 VALUES
 ((SELECT dog_id FROM Dogs WHERE name = 'Max'), '2025-06-10 08:00:00', 30, 'Parklands', 'open'),
 ((SELECT dog_id FROM Dogs WHERE name = 'Bella'), '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted'),
-((SELECT dog_id FROM Dogs WHERE name = 'Rocky'), '2025-06-11 10:00:00', 60, 'Lakeside Trail', 'open'),
+((SELECT dog_id FROM Dogs WHERE name = 'chicken'), '2025-06-11 10:00:00', 60, 'Lakeside Trail', 'open'),
 ((SELECT dog_id FROM Dogs WHERE name = 'Cookies'), '2025-06-11 11:30:00', 30, 'Greenwood Park', 'open'),
 ((SELECT dog_id FROM Dogs WHERE name = 'bull'), '2025-06-11 11:30:00', 30, 'Greenwood Park', 'open'),
 ((SELECT dog_id FROM Dogs WHERE name = 'Sweety'), '2025-06-12 14:00:00', 40, 'City Center Walkway', 'cancelled');
