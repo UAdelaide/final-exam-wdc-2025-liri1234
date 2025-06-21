bd21af16ebc786611b7c00d81c0015dcbbd1a297
Subhashree <unknown@student.adelaide.edu.au>
2025-06-21 03:52:48 +0000

    [automatic commit] 2025-06-21 03:52:48 UTC

--- part2/routes/walkRoutes.js
+++ part2/routes/walkRoutes.js
@@ -60,7 +60,7 @@ router.post('/:id/apply', async (req, res) => {
 });
 
 // GET all dogs (no auth required)
-router.get('/', async (req, res) => {
+router.get('/dogs', async (req, res) => {
   try {
     const [rows] = await db.query('SELECT dog_id, name, size, owner_id FROM Dogs');
     res.json(rows);
